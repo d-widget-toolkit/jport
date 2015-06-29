e90b083303adeb4cf62f1515747826cfe52d51e7
Jacob Carlborg <doob@me.com>
2015-06-29 21:06:13 +0200

    Add tests for for statement with external types.

--- src/test/scala/functional/statements/ForStatementSuite.scala
+++ src/test/scala/functional/statements/ForStatementSuite.scala
@@ -24,6 +24,15 @@ class ForStatementSuite extends Suite {
     java should portFromFileTo("Foo", d)
   }
 
+  test("external type as initializer") {
+    codeToFile("Bar")("public class Bar {}")
+
+    val java = javaCode("for(Bar a = new Bar();;) {}")
+    val d = "import Bar;\n\n" + dCode("for (Bar a = new Bar();;) {}")
+
+    java should portFromFileTo("Foo", d)
+  }
+
   test("for statement with initializer and expression") {
     val java = javaCode("for(int a = 0; a < 10;) {}")
     val d = dCode("for (int a = 0; a < 10;) {}")
@@ -31,10 +40,30 @@ class ForStatementSuite extends Suite {
     java should portFromFileTo("Foo", d)
   }
 
+  test("initializer and external type in expression") {
+    codeToFile("Bar")("public class Bar { int a; }")
+
+    val java = javaCode("for(int a = 0; new Bar().a < 10;) {}")
+    val forStatement = dCode("for (int a = 0; new Bar().a < 10;) {}")
+
+    val d = "import Bar;\n\n" + forStatement
+    java should portFromFileTo("Foo", d)
+  }
+
   test("for statement with initializer, expression and updater") {
     val java = javaCode("for(int a = 0; a < 10; a++) {}")
     val d = dCode("for (int a = 0; a < 10; a++) {}")
 
+    java should portFromFileTo("Foo", d)
+  }
+
+  test("initializer, expression and external type in updater") {
+    codeToFile("Bar")("public class Bar { int a; }")
+
+    val java = javaCode("for(int a = 0; a < 10; new Bar().a++) {}")
+    val forStatement = dCode("for (int a = 0; a < 10; new Bar().a++) {}")
+
+    val d = "import Bar;\n\n" + forStatement
     java should portFromFileTo("Foo", d)
   }
 
@@ -57,6 +86,31 @@ class ForStatementSuite extends Suite {
       """
     }
 
+    java should portFromFileTo("Foo", d)
+  }
+
+  test("with body and external type") {
+    codeToFile("Bar")("public class Bar {}")
+
+    val java = javaCode {
+      """
+      for(int a = 0; a < 10; a++)
+      {
+        Bar b = new Bar();
+      }
+      """
+    }
+
+    val forStatement = dCode {
+      """
+      for (int a = 0; a < 10; a++)
+      {
+          Bar b = new Bar();
+      }
+      """
+    }
+
+    val d = "import Bar;\n\n" + forStatement
     java should portFromFileTo("Foo", d)
   }
 }
